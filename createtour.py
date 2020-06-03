59f1be39e402aaa762682a5e3a4ff83affab12ec
Kevin Stadler <kevin.stadler@ed.ac.uk>
2020-06-03 15:50:55 +0800

    Fix speed, explicit rotations and country names

--- createtour.py
+++ createtour.py
@@ -1,20 +1,27 @@
-#!/usr/local/bin/python3.6
+#!/usr/local/bin/python3
 
 import argparse
 
+import re
 import mmap
-from functools import reduce
+from more_itertools import windowed
 from math import radians, degrees, sin, cos, asin, acos, sqrt, atan2
 
+from os.path import basename
 import simplekml
+import shapely
+from shapely.geometry import asLineString, LineString
+from pycountry import countries
 
-# good combinations: tilt 0 with distance 500, tilt 55 with distance 100
+# good combinations: tilt 0 with distance 500 at 20kmh for vertical trace, tilt 65 with distance 200 and 10-15kmh for 'walking' (but enters ground sometimes)
 
 argparser = argparse.ArgumentParser(description='Generate Google Earth tour(s) from boundary polygons.')
-argparser.add_argument('file', nargs='*', default=['gadm36_AUT_0.kml'], help='boundary polygon .kml file(s) or list filename (.txt) to generate tour(s) for')
-argparser.add_argument('--tilt', type=float, default=45, help='camera angle (0 = vertically above, 90 = parallel to horizon)')
-argparser.add_argument('--distance', type=float, default=500, help='distance from the camera to the point on the ground (in m)')
+argparser.add_argument('file', nargs='*', default=['data/AUT.kmz'], help='boundary polygon .kml file(s) or list filename (.txt) to generate tour(s) for')
+argparser.add_argument('--tilt', type=float, default=90, help='camera angle (0 = vertically above, 90 = parallel to horizon)')
+argparser.add_argument('--distance', type=float, default=3, help='distance from the camera to the point on the ground (in m)')
 argparser.add_argument('--kmh', type=float, default=20, help='animation ground speed (in km/h)')
+argparser.add_argument('--dps', type=float, default=30, help='degrees that the camera rotates per second')
+argparser.add_argument('--ccw', '--left', action='store_true', help='whether the surrounded country should be to the left of the tour (default is clockwise, country to the right)')
 args = argparser.parse_args()
 
 if len(args.file) == 1 and args.file[0].endswith('.txt'):
@@ -25,7 +32,8 @@ print('Generating ' + str(len(args.file)) + ' tour(s)')
 
 def greatcircledistance(p1, p2):
   lon1, lat1, lon2, lat2 = map(radians, [p1[0], p1[1], p2[0], p2[1]])
-  return 6371 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2))
+  # min for overflow of approximation
+  return 6371 * acos(min(1, sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2)))
 
 def getcoords(coordstring):
   return list(map(lambda coord: list(map(float, coord.split(','))), coordstring.split(' ')))
@@ -34,8 +42,16 @@ def takelonger(currentcoords, coordbytearray):
   coords = getcoords(coordbytearray.decode())
   return coords if currentcoords == None or len(coords) > len(currentcoords) else currentcoords
 
+def getheading(fr, to):
+  return degrees(atan2(to[0] - fr[0], to[1] - fr[1]))
+
+def addflytopoint(point, heading, duration = None, distance = args.distance, tilt = args.tilt, mode = 'smooth'):
+  playlist.newgxflyto(gxduration=duration, gxflytomode=mode,
+#    camera=simplekml.Camera(longitude = nxt[0], latitude = nxt[1], altitude = distance, heading = heading, tilt = tilt))
+    lookat=simplekml.LookAt(longitude = point[0], latitude = point[1], range = distance, heading = heading, tilt = tilt))
+
 coordinatestartmarker = b'<Polygon><outerBoundaryIs><LinearRing><coordinates>'
-coordinateendmarker = b'</coordinates></LinearRing></outerBoundaryIs></Polygon>'
+coordinateendmarker = b'</coordinates></LinearRing></outerBoundaryIs>'
 
 for filename in args.file:
   coords = None
@@ -52,14 +68,24 @@ for filename in args.file:
         end = mappedfile.find(coordinateendmarker, offset)
         mappedfile.seek(offset + len(coordinatestartmarker))
         coordinatebytearray = mappedfile.read(end - mappedfile.tell())
-        print('Read polygon spec of ' + str(len(coordinatebytearray)) + ' bytes from file')
+        print('Read polygon spec #' + str(npolys) + ': ' + str(len(coordinatebytearray)) + ' bytes')
         coords = takelonger(coords, coordinatebytearray)
 
-  print('Generating ' + str(len(coords)) + ' point tour from ' + filename + ' (longest of ' + str(npolys) + ' polygon(s))')
+  origcount = len(coords)
+  ring = asLineString(coords)
+  coords = ring.simplify(.00025, False).coords
+  print('Smoothed longest tour from ' + str(origcount) + ' down to ' + str(len(coords)) + ' points')
 
-  # Create an instance of kml
-  kml = simplekml.Kml(name='border line tour', open=1)
+  if (args.ccw):
+    pass # TODO reverse array
 
+  name = ''
+  iso = re.search(r'[A-Z]{3}', basename(filename))
+  if iso:
+    name = countries.get(alpha_3=iso.group(0))
+    name = (name.official_name or name.name) if name else ''
+
+  kml = simplekml.Kml(name=name + ': a border tour', open=1)
   # Create a tour and attach a playlist to it
   tour = kml.newgxtour(name='click here to start the tour!')
   playlist = tour.newgxplaylist()
@@ -68,25 +94,40 @@ for filename in args.file:
   #animatedupdate = playlist.newgxanimatedupdate(gxduration=6.5)
   #animatedupdate.update.change = '<IconStyle targetId="{0}"><scale>10.0</scale></IconStyle>'.format(pnt.style.iconstyle.id)
 
-  setup = playlist.newgxflyto(gxduration=3, gxflytomode='bounce')
-  setup.camera.longitude = coords[0][0]
-  setup.camera.latitude = coords[1][0]
-  setup.camera.altitude = 5000
-  setup.camera.heading = degrees(atan2(coords[1][0] - coords[0][0], coords[1][1] - coords[0][1]))
-  #setup.camera.tilt = 0
-  playlist.newgxwait(gxduration=2)
+  # TODO use GxViewerOptions (streetview) for other projects: https://simplekml.readthedocs.io/en/latest/abstractviews.html#simplekml.GxViewerOptions
 
-  def addpointtoplaylist(prev, cur):
-    duration = args.kmh * greatcircledistance(prev, cur)
-    if args.tilt == 0.0:
-      playlist.newgxflyto(gxduration=duration, gxflytomode='smooth',
-        camera=simplekml.Camera(longitude = cur[0], latitude = cur[1], altitude = args.distance, heading = degrees(atan2(cur[0] - prev[0], cur[1] - prev[1])), tilt = 0))
-    else:
-      playlist.newgxflyto(gxduration=duration, gxflytomode='smooth',
-        lookat=simplekml.LookAt(longitude = cur[0], latitude = cur[1], range = args.distance, heading = degrees(atan2(cur[0] - prev[0], cur[1] - prev[1])), tilt = args.tilt))
+  prvheading = getheading(coords[0], coords[1])
+  addflytopoint(coords[0], prvheading, 4, 5000, 0, 'bounce')
+#  addflytopoint(coords[0], prvheading, 4)
+#  playlist.newgxwait(gxduration=5)
 
-    return cur
-  reduce(addpointtoplaylist, coords)
+  tourduration = 0
+  for prv, cur, nxt in windowed(coords, 3):
+    walktime = 60 * 60 * greatcircledistance(prv, cur) / args.kmh
+    tourduration = tourduration + walktime
+
+    heading = getheading(cur, nxt)
+    rotationtime = abs(heading - prvheading) / args.dps
+    if rotationtime > 0:
+      rotationtime = walktime * min(1, rotationtime / walktime)
+      walktime = walktime - rotationtime
+
+    walktopoint = cur if rotationtime == 0 else LineString([prv, cur]).interpolate(walktime / (walktime + rotationtime), True).coords[0]
+    # earth does NOT like 90 degree turns per second! but lower dps means having to leave the center line wayyy earlier...so might have to adjust speed along line?
+
+    if walktime > 0:
+      addflytopoint(walktopoint, prvheading, walktime)
+    if rotationtime > 0:
+      addflytopoint(cur, heading, rotationtime)
+
+    prvheading = heading
+
+  # TODO add marker point at the first polygon, label: 'the _ border (x km, tour will take y hours z minutes)'
+  # TO your left: _, to your right: Austria
+  duration = str(round(tourduration / 60)) + ' minutes'
+  kml.newpoint(name=name + 'a border tour', description='a walk along the border, total duration ' + duration, coords=[coords[0]])
+  line = kml.newlinestring(name='the border', coords=coords)
+  print('Saving tour file, total duration ' + duration)
 
   # Save to file
-  kml.save(filename + '-tour.kml')
+  kml.save(basename(filename) + '-tour.kml')
