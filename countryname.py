75bac8fb438714603d3a1058c6dd3093a80d3e55
Kevin Stadler <kevin.stadler@ed.ac.uk>
2021-02-02 21:25:33 +0800

    Improved stats and moved country name selection to its own module

--- countryname.py
+++ countryname.py
@@ -2,6 +2,7 @@ from pycountry import countries
 
 # get a nicely verbose name of a country from a geojson feature
 def countryname(feature):
+
   name = countries.get(alpha_3=feature['properties']['tags']['ISO3166-1:alpha3'])
   if name:
     # get some good one
@@ -9,9 +10,14 @@ def countryname(feature):
       name = name.official_name
     except AttributeError:
       name = name.name
-  else:
+
+    # don't accept crappy names
+    if ',' in name:
+      name = None
+
+  if name == None:
     # revert to the OSM feature, take first that is there
-    for key in ['official_name:en', 'official_name', 'int_name', 'name:en']:
+    for key in ['official_name:en', 'int_name', 'name:en', 'official_name']:
       try:
         name = feature['properties']['tags'][key]
         # if it worked, break
@@ -20,6 +26,6 @@ def countryname(feature):
         continue
   # sometimes prepend 'The' (https://www.grammar-quizzes.com/article4c.html)
   # skip first letter because it might be upper or lower case 'the'
-  if not name.startswith('he ', 1) and any([part in name for part in ['Commonwealth', 'Democratic', 'Duchy', 'Federation', 'Holy', 'Kingdom', 'Principality', 'Republic', 'Union', 'United']]): # capture Union and United
+  if not name.startswith('he ', 1) and any([part in name for part in ['Commonwealth', 'Democratic', 'Duchy', 'Federation', 'Holy', 'Kingdom', 'Principality', 'Republic', 'State of', 'Union', 'United']]):
     name = 'The ' + name
   return name
--- stats.py
+++ stats.py
@@ -2,62 +2,85 @@
 
 from functools import reduce
 from more_itertools import pairwise
+from operator import itemgetter
 from os.path import basename, splitext
 import re
+import csv
 
 import argparse
 
 import geojson
-from pycountry import countries
 from great_circle_calculator.great_circle_calculator import distance_between_points as gcdistance
+from area import area
+
+from countryname import countryname
 
 argparser = argparse.ArgumentParser(description='generate some information about (multi)polygons from geojson files')
 argparser.add_argument('file', nargs='*', help='geojson file(s)')
-argparser.add_argument('-parts', action='store_true', help='write one line per part, not just one line per administrative body')
+argparser.add_argument('-parts', action='store_true', default=True, help='write one line per part, not just one line per administrative body')
 argparser.add_argument('-summary', action='store_true', help='display aggregate information about each borders largest polygon')
 
 import percache
-cache = percache.Cache('borderstatscache')
+cache = percache.Cache('stats.pycache')
 
 # in km
 def ringlength(coords):
   return sum([ gcdistance(pt1, pt2, 'kilometers') for pt1, pt2 in pairwise(coords + coords[:1]) ])
 
+def sqkm(geometry):
+  return area(geometry) / 1000000
+
 # returns an array of tuples, where the first element is the length of a part and the second an array of holes in it (again respectively their length)
-def getpartsandholes(coords):
-  if isinstance(coords[0][0], list):
+def getpartsandholes(geometry):
     # multipolygon
-    outer, *holes = coords
-    return (ringlength(outer), [ ringlength(hole) for hole in holes ])
-  else:
-    return (ringlength(coords), [])
+  outer, *holes = geometry['coordinates']
+  return (ringlength(outer), sqkm(geometry), [ ringlength(hole) for hole in holes ])
 
 @cache
 def getpartsandholesfromfile(filename):
   with open(filename) as f:
     fts = geojson.load(f).features
-    name = fts[0]['properties']['geocoding']['name'] if 'geocoding' in fts[0]['properties'] else filename
+#    name = fts[0]['properties']['geocoding']['name'] if 'geocoding' in fts[0]['properties'] else filename
     if len(fts) > 1:
-      print(f"More than one feature in {name} result!?")
-    return [ getpartsandholes(pt) for pt in fts[0].geometry.coordinates ]
+      print(f'More than one feature in {filename}!?')
+    # parts are sorted descending based on outer ring (perimeter) length
+    return { 'iso': fts[0]['properties']['tags']['ISO3166-1:alpha2'], 'name': countryname(fts[0]), 'parts': sorted([ getpartsandholes({ 'type': 'Polygon', 'coordinates': pt }) for pt in fts[0].geometry.coordinates ], key=itemgetter(0), reverse=True) }
 
-def getpartsandholesfromcountry(filename):
-  name = splitext(basename(filename))[0]
-  fullname = name
-  iso = re.search(r'[A-Z]{3}', name)
-  if iso:
-    fullname = countries.get(alpha_3=iso.group(0))
-    fullname = (fullname.name or fullname.official_name).replace("'", "\\'") if fullname else filename
-  return (fullname, getpartsandholesfromfile(filename))
+#@cache
+#def getpartsandholesfromcountry(filename):
+#  name = splitext(basename(filename))[0]
+#  fullname = name
+#  iso = re.search(r'[A-Z]{3}', name)
+#  if iso:
+#    fullname = countries.get(alpha_3=iso.group(0))
+#    fullname = (fullname.name or fullname.official_name).replace("'", "\\'") if fullname else filename
+#  return (fullname, getpartsandholesfromfile(filename))
 
 args = argparser.parse_args()
 
-allstats = { key:value for (key, value) in [ getpartsandholesfromcountry(filename) for filename in args.file] }
-longestouterring = [ max([ part[0] for part in parts ]) for parts in allstats.values() ]
+allstats = [ getpartsandholesfromfile(filename) for filename in args.file]
+
 if args.summary:
+  longestouterring = [ max([ part[0] for part in country['parts'] ]) for country in allstats ]
   print(f'{len(allstats)} borders, total length of each parts longest outer ring is {round(sum(longestouterring), 2)}, ranging from {round(min(longestouterring), 2)} to {round(max(longestouterring), 2)}')
+
 else:
-  for (name, parts) in allstats.items():
-    # 1. name 2. number of parts 3. number of holes 4. total length of all outer rings 5. length of longest outer ring
-  #  print(f'{name}: {len(parts)} part(s) with a total length of {round(sum(part[0] for part in parts))}km (with {sum([len(part[1]) for part in parts])} holes in it), single longest part is {round(max([part[0] for part in parts]))}km long')
-    print(f'{name};{len(parts)};{sum([ len(part[1]) for part in parts ])};{round(sum(part[0] for part in parts), 2)};{round(max([ part[0] for part in parts ]), 2)}')
+  if args.parts:
+    print('iso;name;videos;holes;perimeter;area')
+    with open('videos.csv') as f:
+      videos = { iso: ids for (iso, ids) in csv.reader(f) }
+  else:
+    print('name;nparts;holes;totalperimeter;longestperimeter;totalarea;largestarea')
+
+  for country in allstats:
+    if args.parts:
+      nameprinted = False
+      try:
+        video = videos[country['iso']]
+      except KeyError:
+        video = ''
+      for part in country['parts']:
+        print(f"{country['iso']};{'' if nameprinted else country['name']};{'' if nameprinted else video};{len(part[2])};{round(part[0], 4)};{round(part[1], 4)}")
+        nameprinted = True
+    else:
+      print(f"{country['name']};{len(parts)};{sum([ len(part[2]) for part in parts ])};{round(sum(part[0] for part in parts), 4)};{round(max([ part[0] for part in parts ]), 4)};{round(sum(part[1] for part in parts), 2)};{round(max([ part[1] for part in parts ]), 2)}")
