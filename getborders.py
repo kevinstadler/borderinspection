74d0af9fb6ca4a738fd3f527b091147f2b900a4c
Kevin Stadler <kevin.stadler@ed.ac.uk>
2021-02-02 21:26:18 +0800

    Query borders based on OSM consensus, not ISO lists

--- getborders.py
+++ getborders.py
@@ -3,20 +3,24 @@
 import json
 from os import path, rename
 from osm2geojson import json2geojson, overpass_call
-import pycountry
 import time
 
 admin_level = 2
 
-for c in pycountry.countries:
-  filename = f'{c.alpha_3}.geojson'
-  if path.exists(f'data/2/{filename}') or path.exists(f'data/3/{filename}') or path.exists(f'data/4/{filename}') or path.exists(f'data/6/{filename}'):
+# go by what OSM thinks about de-facto states
+iso = overpass_call(f'[out:csv("ISO3166-1:alpha3")];rel[admin_level={admin_level}]["ISO3166-1:alpha3"];out;').split()[1:]
+print(f"Checking for {len(iso)} countries' borders")
+
+for c in iso:
+  filename = f'{c}.geojson' #c.alpha_3
+  if path.exists(f'data/2/{filename}'):# or path.exists(f'data/3/{filename}') or path.exists(f'data/4/{filename}') or path.exists(f'data/6/{filename}'):
     continue
   print(c)
   # alternatively: don't filter for admin_level, but check if it's 2 (country) or 3 (overseas something)
   # filter out boundary=claimed_administrative
   # land masses or boundary=claimed_administrative areas are sometimes also tagged with the ISO codes, so best to ask for admin_level explicitly AND set type != land_area
-  r = json.loads(overpass_call(f'[out:json];(rel["ISO3166-1:alpha2"={c.alpha_2}];rel["ISO3166-1:alpha3"={c.alpha_3}];)->.a;rel.a[boundary=administrative][admin_level={admin_level}][type!=land_area];out geom;'))
+  r = json.loads(overpass_call(f'[out:json];rel["ISO3166-1:alpha3"={c}][boundary=administrative][admin_level={admin_level}][type!=land_area];out geom;'))
+#  r = json.loads(overpass_call(f'[out:json];(rel["ISO3166-1:alpha2"={c.alpha_2}];rel["ISO3166-1:alpha3"={c.alpha_3}];)->.a;rel.a[boundary=administrative][admin_level={admin_level}][type!=land_area];out geom;'))
   if len(r['elements']) == 0:
     print('empty result!')
     continue
