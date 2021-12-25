13e7c6602c3dbd43de171bc4586d598e2470a778
kevinstadler <kevinstadler@users.noreply.github.com>
2021-12-25 22:37:06 +0100

    Write landuse stats to json files

--- stats.py
+++ stats.py
@@ -11,7 +11,9 @@ import re
 import csv
 
 import argparse
+from os import path
 
+import json
 import geojson
 from great_circle_calculator.great_circle_calculator import distance_between_points as gcdistance
 from area import area, polygon__area, ring__area
@@ -47,25 +49,28 @@ def polygonsqkm(coordinates):
 def sqkm(geometry):
   return area(geometry) / 1000000
 
-def addpolygonstats(geometry):
-  # multipolygon
+def getpolygonstats(geometry):
   outer, *holes = geometry['coordinates']
-  # tuple elements:
-  # [0] original geometry
-  # [1] outer polygon (border) length
-  # [2] area in sqkm
-  # [3] array of (length, sqkm) tuples, for each hole of the polygon
-  return {'geometry': geometry, 'length': ringlength(outer), 'fullarea': ringsqkm(outer), 'effectivearea': sqkm(geometry), 'holes': [ { 'length': ringlength(hole), 'area': ringsqkm(hole) } for hole in holes ]}
+  # outer polygon (border) length
+  # area in sqkm
+  # array of (length, sqkm) tuples, for each hole of the polygon
+  return { 'length': ringlength(outer), 'fullarea': ringsqkm(outer), 'effectivearea': sqkm(geometry), 'holes': [ { 'length': ringlength(hole), 'area': ringsqkm(hole) } for hole in holes ]}
 
 #@cache
 def getpartsandholesfromfile(filename):
+  print(f'\nLoading {filename}...')
   with open(filename) as f:
     fts = geojson.load(f).features
 #    name = fts[0]['properties']['geocoding']['name'] if 'geocoding' in fts[0]['properties'] else filename
     if len(fts) > 1:
-      print(f'More than one feature in {filename}!?')
+      raise ValueError(f'More than one feature in {filename}!?')
     # parts are sorted descending based on outer ring (perimeter) length
-    return { 'iso': fts[0]['properties']['tags']['ISO3166-1:alpha2'], 'name': countryname(fts[0]), 'parts': sorted([ addpolygonstats({ 'type': 'Polygon', 'coordinates': pt }) for pt in fts[0].geometry.coordinates ], key=itemgetter('length'), reverse=True) }
+    parts = [{ 'type': 'Polygon', 'coordinates': pt } for pt in fts[0].geometry.coordinates ]
+    stats = [ getpolygonstats(pt) for pt in parts ]
+    partorder = [ i for i, pt in sorted(enumerate([s['length'] for s in stats ]), key=itemgetter(1), reverse=True) ]
+    if len(partorder) > 1:
+      print(f"Part order: {partorder}")
+    return { 'info': { 'iso2': fts[0]['properties']['tags']['ISO3166-1:alpha2'], 'iso3': fts[0]['properties']['tags']['ISO3166-1:alpha3'], 'name': countryname(fts[0]) }, 'parts': [ parts[i] for i in partorder ], 'stats': [ stats[i] for i in partorder ] }
 
 #@cache
 #def getpartsandholesfromcountry(filename):
@@ -80,8 +85,6 @@ def getpartsandholesfromfile(filename):
 args = argparser.parse_args()
 
 if args.landuse:
-    igbp = { 1: '#008000', 2: '#0f0', 3: '#9c0', 4: '#9f9', 5: '#396', 6: '#936', 7: '#fc9', 8: '#cfc', 9: '#fc0', 10: '#f90', 11: '#069', 12: '#ff0', 13: '#f00', 14: '#996', 15: '#fff', 16: '#808080', 17: '#000080', 18: '#00f', 100: '#000' } # 18 is inland
-
     # TODO prevent Pillow from loading the entire image by cropping it
     Image.MAX_IMAGE_PIXELS = None
     igbp18 = Image.open('data/glccgbe20_tif/gbigbpgeo20.tif')
@@ -117,121 +120,110 @@ if args.landuse:
       else:
         return xy
 
+def sign(a): # returns -1, 0 and 1
+  return (a > 0) - (a < 0)
+
 with open("data/summary.csv", 'w') as summary:
   for filename in args.file:
+
     country = getpartsandholesfromfile(filename)
     if args.summary:
       longestouterring = [ max([ part[0] for part in country['parts'] ]) for country in allstats ]
       print(f'{len(allstats)} borders, total length of each parts longest outer ring is {round(sum(longestouterring), 2)}, ranging from {round(min(longestouterring), 2)} to {round(max(longestouterring), 2)}')
 
-    else:
-      if args.landuse:
-        country['landuses'] = []
+    runparts = False
+    if args.landuse:
+      landusefile = f'data/landuse/{country["info"]["iso3"]}.json'
+      if path.exists(landusefile):
+        print("Landuse file already exists, skipping")
+      else:
+        runparts = True
+        print("Computing landuse...")
+        landuses = { 'info': country['info'], 'stats': country['stats'], 'landuses': [] }
 
-      def sign(a): # returns -1, 0 and 1
-        return (a > 0) - (a < 0)
-#    print('iso;name;videos;holes;perimeter;area')
-#  else:
-#    print('name;nparts;holes;totalperimeter;longestperimeter;totalarea;largestarea')
-      with open(f"data/{country['iso']}.csv", 'w') as file:
-        for part in country['parts']:
+    if runparts:
+      try:
+        for n, part in enumerate(country['parts']):
+          print(f"Part {n+1}...")
           #print(f"\n\n{len(part['holes'])};{round(part['length'], 4)};{round(part['effectivearea'], 4)}")
           if args.landuse:
-              ring = part['geometry']['coordinates'][0]
-              totaldist = ringlength(ring)
-              cumdist = 0
-              lastxy, curlanduse = getpointinfo(ring[0])
-              prev = ring[0] # prev can be either a vertex of the border, or a pixel cutting point we found somewhere?
-              landuses = []
-              for vertex in ring:
-                xy = lonlattoxy(vertex)
-                if xy == lastxy:
-                  cumdist += distance(prev, vertex)
-                  prev = vertex
-                  # vertex doesn't cross pixel boundary, just carry on
-                  continue
-
-                # else: find boundaries between prev and current vertex (might be multiple)
-                segment = LineString([prev, vertex])
-                longaxis = 0 if abs(xy[0] - lastxy[0]) >= abs(xy[1] - lastxy[1]) else 1 # 0 = x, 1 = y
-                shortaxis = (longaxis + 1) % 2
-                dirs = [ sign(xy[i] - lastxy[i]) for i in range(2) ]
-                mockdirs = [ 1 if dir == 0 else dir for dir in dirs ]
-                # print(f'\n{lastxy} + {dirs} ({mockdirs}) -> {xy} (long axis={"x" if longaxis == 0 else "y"})')
-                for i in range(lastxy[longaxis], xy[longaxis] + mockdirs[longaxis], mockdirs[longaxis]):
-                  covered = False
-                  for j in range(lastxy[shortaxis], xy[shortaxis] + mockdirs[shortaxis], mockdirs[shortaxis]):
-                    # check intersect with a hook in the corner of where we're going
-                    hooks = gethook((i, j) if longaxis == 0 else (j, i), mockdirs)
-                    # FIXME gotta check TOUCHES first
-                    intersections = [ segment.intersection(hook) for hook in hooks ]
-                    try:
-                      point = next(( p for p in intersections if not p.is_empty ))
-                    except StopIteration:
-                      if segment.touches(hooks[0]) or segment.touches(hooks[1]):
-                        raise ValueError("Two segments don't intersect, but they do touch?")
-                      # no intersection, just carry on...
-                      if covered:
-                        break
-                      else:
-                        continue
-
-                    covered = True
-                    point = point.coords[0]
-                    cumdist += distance(prev, point)
-                    prev = point
-                    # get landuse of next entered cell
-                    intdirs = [ 0 if intersection.is_empty else 1 for intersection in intersections ]
-                    #if intdirs[0] == intdirs[1]: # FIXME check these here, if everything is correct...
-                      #print("IN BOTH DIRECTIONS")
-                      #print(intersections[0])
-                      #print(intersections[1])
-                    lastxy = (lastxy[0] + dirs[0]*intdirs[0], lastxy[1] + dirs[1]*intdirs[1])
-                    #print(f'->{lastxy}', end='')
-                    try:
-                      nextlanduse = getlanduse(lastxy)
-                    except IndexError:
-                      print(prev)
-                      print(lastxy)
-                      raise ValueError("FOOL")
-                    if nextlanduse != curlanduse:
-                      landuses.append((cumdist, curlanduse))
-                      curlanduse = nextlanduse
-                      #print(f'{cumdist}\n{curlanduse},', end='')
-                  if lastxy == xy:
-                    break # FIXME break two actually
-                  if not covered:
-                    print(list(segment.coords))
-                    print(list(hooks[0].coords))
-                    print(list(hooks[1].coords))
-                    raise ValueError("NOT COVERED??")
-                #landuses.append((distancealong, landuse))
-
+            ring = part['coordinates'][0]
+            totaldist = ringlength(ring)
+            cumdist = 0
+            lastxy, curlanduse = getpointinfo(ring[0])
+            prev = ring[0] # prev can be either a vertex of the border, or a pixel cutting point we found somewhere?
+            landuse = []
+            for vertex in ring:
+              xy = lonlattoxy(vertex)
+              if xy == lastxy:
                 cumdist += distance(prev, vertex)
                 prev = vertex
-
-                # can't trust the un-normalized distance (not in km)!
-                #linearring.project(Point(vertex), True)
-                #print(f'{cumdist}: {getlanduse(vertex)}')
-                # next in same pixel -> go on, else: find new pixel
-              # just round it off at the end (final point)
-              landuses.append((cumdist, curlanduse))
-              country['landuses'].append(landuses)
-
-        if args.landuse:
-          pxperkm = 0.2
-          print('<div>')
-          for part in country['landuses']:
-            print(f'<div style="height: {round(part[-1][0]*pxperkm)}px; background: linear-gradient(', end='')
-            curpx = 0
-            curuse = part[0][1]
-            print(igbp[curuse], end='')
-            for dist, use in part:
-              nextpx = round(dist*pxperkm)
-              if nextpx == curpx:
+                # vertex doesn't cross pixel boundary, just carry on
                 continue
-              print(f', {igbp[use]} {curpx}px, {igbp[use]} {nextpx}px', end='')
-              curpx = nextpx
-            print(f', {igbp[use]})"></div>')
-          print('</div>')
-#          print(f"{country['name']};{len(parts)};{sum([ len(part[2]) for part in parts ])};{round(sum(part[0] for part in parts), 4)};{round(max([ part[0] for part in parts ]), 4)};{round(sum(part[1] for part in parts), 2)};{round(max([ part[1] for part in parts ]), 2)}")
+
+              # else: find boundaries between prev and current vertex (might be multiple)
+              segment = LineString([prev, vertex])
+              longaxis = 0 if abs(xy[0] - lastxy[0]) >= abs(xy[1] - lastxy[1]) else 1 # 0 = x, 1 = y
+              shortaxis = (longaxis + 1) % 2
+              dirs = [ sign(xy[i] - lastxy[i]) for i in range(2) ]
+              mockdirs = [ 1 if dir == 0 else dir for dir in dirs ]
+              # print(f'\n{lastxy} + {dirs} ({mockdirs}) -> {xy} (long axis={"x" if longaxis == 0 else "y"})')
+              for i in range(lastxy[longaxis], xy[longaxis] + mockdirs[longaxis], mockdirs[longaxis]):
+                covered = False
+                for j in range(lastxy[shortaxis], xy[shortaxis] + mockdirs[shortaxis], mockdirs[shortaxis]):
+                  # check intersect with a hook in the corner of where we're going
+                  hooks = gethook((i, j) if longaxis == 0 else (j, i), mockdirs)
+                  # FIXME gotta check TOUCHES first
+                  intersections = [ segment.intersection(hook) for hook in hooks ]
+                  try:
+                    point = next(( p for p in intersections if not p.is_empty ))
+                  except StopIteration:
+                    if segment.touches(hooks[0]) or segment.touches(hooks[1]):
+                      raise ValueError("Two segments don't intersect, but they do touch?")
+                    # no intersection, just carry on...
+                    if covered:
+                      break
+                    else:
+                      continue
+
+                  covered = True
+                  point = point.coords[0]
+                  cumdist += distance(prev, point)
+                  prev = point
+                  # get landuse of next entered cell
+                  intdirs = [ 0 if intersection.is_empty else 1 for intersection in intersections ]
+                  #if intdirs[0] == intdirs[1]: # FIXME check these here, if everything is correct...
+                    #print("IN BOTH DIRECTIONS")
+                    #print(intersections[0])
+                    #print(intersections[1])
+                  lastxy = (lastxy[0] + dirs[0]*intdirs[0], lastxy[1] + dirs[1]*intdirs[1])
+                  #print(f'->{lastxy}', end='')
+                  nextlanduse = getlanduse(lastxy)
+                  if nextlanduse != curlanduse:
+                    landuse.append((round(cumdist, 2), curlanduse, (round(prev[0], 3), round(prev[1], 3))))
+                    curlanduse = nextlanduse
+                    #print(f'{cumdist}\n{curlanduse},', end='')
+                if lastxy == xy:
+                  break # FIXME break two loops actually
+                if not covered:
+                  print(list(segment.coords))
+                  print(list(hooks[0].coords))
+                  print(list(hooks[1].coords))
+                  raise ValueError("NOT COVERED??")
+              #landuses.append((distancealong, landuse))
+
+              cumdist += distance(prev, vertex)
+              prev = vertex
+
+              # can't trust the un-normalized distance (not in km)!
+              #linearring.project(Point(vertex), True)
+              #print(f'{cumdist}: {getlanduse(vertex)}')
+              # next in same pixel -> go on, else: find new pixel
+            # just round it off at the end (final point)
+            landuse.append((round(cumdist, 2), curlanduse, prev))
+            landuses['landuses'].append(landuse)
+        if args.landuse:
+          with open(landusefile, 'w') as out:
+            json.dump(landuses, out)
+      except ValueError:
+        print("Some point wasn't covered, continueing with next country...")
